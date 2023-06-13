10f769a96dbc4d198f8f8d06d360cbc783b980cf
rubysln <prostosekundu48@gmail.com>
2023-06-13 23:38:03 +0300

    Утверждена новая логика построения БД

--- Создание таблиц.sql
+++ Создание таблиц.sql
@@ -1,6 +1,6 @@
 CREATE TABLE provider -- Поставщики
 (
-	supplier_id int PRIMARY KEY, -- Идентификатор поставщика
+	provider_id int PRIMARY KEY,
 	provied_name varchar(128) NOT NULL, -- Название поставщика
 	adress text NOT NULL, -- Адрес поставщика
 	phone varchar(20) -- Номер телефона поставщика
@@ -8,18 +8,15 @@ CREATE TABLE provider -- Поставщики
 
 CREATE TABLE details -- Детали
 (
-	part_id int PRIMARY KEY, -- Идентификатор детали
+	article varchar(12) PRIMARY KEY,
 	name_detail varchar(64) NOT NULL, -- Название детали
-	article varchar(12) NOT NULL -- Артикул детали
+	fk_details_provider int REFERENCES provider(provider_id)
 );
 
 CREATE TABLE purchases -- Покупки
 (
-	purchases_id int PRIMARY KEY, -- Идентификатор покупки
-	purchase_date date NOT NULL, -- Дата покупки
-	quantity int NOT NULL, -- Количество приобретенных товаров
-	fk_purchases_supplier int REFERENCES provider(supplier_ID), -- Идентификатор поставщика, внешний ключ
-	fk_purchases_details int REFERENCES details(part_id) -- Идентификатор детали, внешний ключ
+	sale_ID int PRIMARY KEY, -- ID продажи
+	sale_date date NOT NULL -- Дата продажи
 );
 
 CREATE TABLE price_history -- История цен
@@ -27,5 +24,12 @@ CREATE TABLE price_history -- История цен
 	price_id int PRIMARY KEY, -- Идентификатор записи цены, первичный ключ
 	price decimal(10, 2) NOT NULL, -- Цена детали
 	change_date date NOT NULL, -- Дата изменения цены
-	fk_price_history_details int REFERENCES details(part_id) NOT NULL -- Идентификатор детали, внешний ключ
+	fk_price_history_details varchar(12) REFERENCES details(article) NOT NULL -- Идентификатор детали, внешний ключ
+);
+
+CREATE TABLE details_purchases -- Многие ко многим - детали <-> покупки
+(
+	fk_details_purchases_details varchar(12) REFERENCES details(article), -- Внешний ключ к деталям
+	fk_details_purchases_purchases int REFERENCES purchases(sale_ID), -- Внешний ключ к покупкам
+	quantity int NOT NULL -- Количество приобретенных товаров
 )
